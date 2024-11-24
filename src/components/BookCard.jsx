d14839a7246e23240c13244c08ce6962b93dcdaf
HongNT03 <nguyentienhong220903@gmail.com>
2024-11-24 16:34:14 +0700

    Update BookCard.jsx

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const BookCard = ({ book }) => {
   return (
-    <div className="p-4 border border-gray-300 rounded shadow">
+    <div className="p-4 h-auto hover:bg-gray-300 border border-gray-300 rounded shadow">
       <Link
         to={{
           pathname: `/book/${book.id}`,
